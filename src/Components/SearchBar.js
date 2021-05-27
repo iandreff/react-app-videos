f2034834d7d6c3b01a6f16a2e5539178d11367e7
Ygor Andreff <iandreff@gmail.com>
2021-05-27 12:35:45 -0600

    Added a search form

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -4,7 +4,15 @@ class SearchBar extends React.Component {
     state = {  }
     render() { 
         return (
-            <div>SearchBar</div>
+            <div className="search-bar ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label className="">Video Search</label>
+                        <input type="text"/>
+                    </div>
+
+                </form>
+            </div>
           );
     }
 }
